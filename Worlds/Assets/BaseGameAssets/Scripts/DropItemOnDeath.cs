cefbfdabf35d2860a7fd59af516e714dc83411a8
onesketchyguy <34846360+onesketchyguy@users.noreply.github.com>
2020-05-10 22:29:38 -0700

    Added rock

---
+++ Worlds/Assets/BaseGameAssets/Scripts/DropItemOnDeath.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Worlds
+{
+    public class DropItemOnDeath : MonoBehaviour
+    {
+        public GameObject[] itemsToDrop;
+
+        [Tooltip("The health to track.")]
+        public HealthManager health;
+
+        private void Start()
+        {
+            health.onDeathCallback += () =>
+            {
+                // Create objects
+
+                foreach (var item in itemsToDrop)
+                {
+                    ObjectManager.localInstance.GetObject(item, transform.position + Vector3.up);
+                }
+            };
+        }
+    }
+}
---
+++ Worlds/Assets/BaseGameAssets/Scripts/ReviveAfterDeath.cs
@@ -0,0 +1,35 @@
+﻿using Mirror;
+using UnityEngine;
+
+namespace Worlds
+{
+    public class ReviveAfterDeath : NetworkBehaviour
+    {
+        [Tooltip("The health to track.")]
+        public HealthManager health;
+
+        public float reviveTime = 1;
+        public float randomRange = 0.35f;
+
+        private void OnValidate()
+        {
+            if (reviveTime - randomRange < 0) randomRange = reviveTime;
+            if (reviveTime + randomRange < 0) randomRange = -reviveTime;
+        }
+
+        private void Start()
+        {
+            health.onDeathCallback += () =>
+            {
+                // Revive after X seconds
+                Invoke(nameof(Revive), Random.Range(reviveTime - randomRange,
+                    reviveTime + randomRange));
+            };
+        }
+
+        public void Revive()
+        {
+            health.RpcReset();
+        }
+    }
+}
